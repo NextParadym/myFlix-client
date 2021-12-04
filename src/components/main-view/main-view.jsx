87acc03bf08a932134af8ef1341bbe15f354fe68
Dr. Lawrence Ibeh <drlawrenceibeh@gmail.com>
2021-12-04 18:33:25 +0100

    Updated

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,11 +3,12 @@
 import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+//import spiritedAway from '../../images/spiritedAway.jpg'
 
 // Exposing a component makes it available for use by other components
 // The class MainView extends React.Component {...}.creates the MainView component.
 
-let imgPath = './img/';
+//let imgPath = './images';
 
 let  movies = [
   { _id :   "6197b6235ba878fd0afa7411", 
@@ -18,9 +19,14 @@ let  movies = [
   Director : {
     Name: "Hayao Miyazaki"},
   Description:   "A girl and her family visit an amusement park. The parents of the girl are turned into pigs, and she is kidnapped by ghosts. ",
-  ImagePath: imgPath +  "spiritedAway.jpg"},
-  
-  { _id :   "6197b6235ba878fd0afa7411", 
+  //ImagePath:  "/images/spiritedAway.jpg" },
+  ImagePath:"https://images-na.ssl-images-amazon.com/images/I/71+0HKU5mJL.jpg"},
+
+  //ImagePath: imgPath +  "spiritedAway.jpg"},
+  //import spiritedAway from '../../images/spiritedAway.jpg'
+
+
+  { _id :   "6197b6235ba878fd0afa7412", 
   Title:  "Harry Potter and the Sorcerer's Stone", 
   Genre:  {
     Name : "Family"
@@ -28,7 +34,7 @@ let  movies = [
   Director : {
     Name: "Chris Columbus"},
   Description: "An eleven-year old boy goes to wizard school for a year and doesn't cast a single spell onscreen the entire time.", 
-  ImagePath: imgPath +  "harryPotterAndTheSorcerersStone.jpg"},
+  ImagePath: <img src = "/images/spiritedAway.jpg" alt = ""/> },
 
   { _id :   "6197bc855ba878fd0afa7418 ", 
   Title: "The Lion King", 
@@ -38,7 +44,9 @@ let  movies = [
   Director : {
     Name: "Rob Minkoff"},
   Description: "Animated lions perform a completely faithful rendition of Hamlet.",  
-  ImagePath: imgPath +  "theLionKing.jpg"} ]
+  //ImagePath: imgPath +  "theLionKing.jpg"}
+  ImagePath: <img src = "/images/spiritedAway.jpg" alt = ""/> },
+]
 
   export default class MainView extends React.Component {
 
