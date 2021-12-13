05e3578047b829ec406bd84c1e9396e0f9a76ea9
Dr. Lawrence Ibeh <drlawrenceibeh@gmail.com>
2021-12-13 07:46:12 +0100

    link login with main-vie, import PropTypes

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from "prop-types";
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
@@ -9,7 +10,7 @@ export function LoginView(props) {
     console.log(username, password);
     /* Send a request to the server for authentication */
     /* then call props.onLoggedIn(username) */
-    // props.onLoggedIn(username);
+    props.onLoggedIn(username);
   };
 
   return (
@@ -26,3 +27,11 @@ export function LoginView(props) {
     </form>
   );
 }
+
+LoginView.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+  }),
+  onLoggedIn: PropTypes.func.isRequired,
+};
