440e3bce2d8f5db4e43c69f7548381e61f17cea1
Dr. Lawrence Ibeh <drlawrenceibeh@gmail.com>
2021-12-14 14:08:27 +0100

    Update and Formating

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-// a requirement for creating a component
+//A requirement for creating a component
 //blueprint for creating new <components className=""></components>
 import React from 'react';
 import axios from 'axios';
@@ -11,52 +11,43 @@ import { MovieCard } from '../movie-card/movie-card';
 //import the movie-view into the main-view
 import { MovieView } from '../movie-view/movie-view';
 
-
 // Exposing a component makes it available for use by other components
 // The class MainView extends React.Component {...}.creates the MainView component.
-
-let imgPath = './img';
-
-  export default class MainView extends React.Component {
-
-    constructor() {
-      super();
+export default class MainView extends React.Component {
+  constructor() {
+    super();
   // Initial state is set to null
-      this.state = {
-        movies: [],
-        selectedMovie: null,
-        user: null,
-        register: null,
-
-      }
-    } 
+  this.state = {
+    movies: [],
+    selectedMovie: null,
+    user: null,
+    register: null,
+  }
+  } 
     
-    componentDidMount(){
-      axios.get('https://murmuring-bastion-72555.herokuapp.com/movies')
-            .then(response => {
-              this.setState({
-                movies: response.data
-              });
-            })
- 
-            .catch(error => {
-              console.log(error);
-            });
+  componentDidMount(){
+    axios.get('https://murmuring-bastion-72555.herokuapp.com/movies')
+    .then(response => {
+      this.setState({
+        movies: response.data
+      });
+    })
 
-        }
+    .catch(error => {
+      console.log(error);
+    });
+  }
         
-    /*When a movie is clicked, this function is invoked and updates 
-    the state of the `selectedMovie` *property to that movie*/
-  
-    setSelectedMovie(newSelectedMovie) {
+  /*When a movie is clicked, this function is invoked and updates 
+  the state of the `selectedMovie` *property to that movie*/
+  setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie
     });
   }
 
   /* When a user successfully logs in, this
-   function updates the `user` property in state to that *particular user*/
-
+  function updates the `user` property in state to that *particular user*/
   onLoggedIn(user) {
     this.setState({
       user
@@ -71,22 +62,20 @@ onRegistration(register) {
 }
 
 /* When a user successfully logs in, this function updates the `user`
-  property in state to that *particular user*/
-  onLoggedIn(user) {
-    this.setState({
-      user
-    });
-  }
+property in state to that *particular user*/
+onLoggedIn(user) {
+  this.setState({
+    user
+  });
+}
 
 //returns the visual representation of the component
 // a requirement for creating a component
 //blueprint for creating new components.
-
 render() {
   const { movies, selectedMovie, user, register } = this.state;
   if (!register) return (<RegistrationView onRegistration={(register) => this.onRegistration(register)}/>);
   
-
   /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
   if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
