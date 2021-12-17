c713689b94920398ba494f63bcbb941469ded42f
Dr. Lawrence Ibeh <drlawrenceibeh@gmail.com>
2021-12-17 16:01:43 +0100

    movie-view-update

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -23,21 +23,21 @@ export class MovieView extends React.Component {
         <div className="movie-title">
           <span className="label">Title: </span>
           <span className="value">{movie.Title}</span>
-          </div>
+        </div>
           <div className="movie-genre">
             <span className="label">Genre: </span>
             <span className="value">{movie.Genre.Name}</span>
-            </div>
+          </div>
             <div className="movie-director">
               <span className="label">Director: </span>
               <span className="value">{movie.Director.Name}</span>
-              </div>
+            </div>
               <div className="movie-description">
                 <span className="label">Description: </span>
                 <span className="value">{movie.Description}</span>
-                </div>
+              </div>
                 <button onClick={() => { onBackClick(null); }}>Back</button>
-                </div>
-                );
+    </div>
+    );
   }
 }
