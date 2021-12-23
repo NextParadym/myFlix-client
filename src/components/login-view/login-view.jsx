67975d313eeaaa317e26232cca229e038b5d04bd
Dr. Lawrence Ibeh <drlawrenceibeh@gmail.com>
2021-12-23 13:35:29 +0100

    Reverse-auth-axions

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,30 +1,21 @@
 import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import PropTypes from 'prop-types';
-import {Form, Button, Card, CardGroup, Container, Row, Col } from 'react-bootstrap';
-import axios from 'axios';
+	import Form from 'react-bootstrap/Form';
+	import Button from 'react-bootstrap/Button';
+	import PropTypes from 'prop-types';
+	import {Form, Button, Card, CardGroup, Container, Row, Col } from 'react-bootstrap';
+	
 
-export function LoginView(props) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+	export function LoginView(props) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+	
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(username, password);
+	    // Send a request to the server for authentication, then call props.onLoggedIn(username)
+      props.onLoggedIn(username);
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  
-    /* Send a request to the server for authentication */
-    axios.post('https://murmuring-bastion-72555.herokuapp.com/login', {
-      Username: username,
-      Password: password
-    })
-    .then(response => {
-      const data = response.data;
-      props.onLoggedIn(data);
-    })
-    .catch(e => {
-      console.log('no such user')
-    });
-  };
   
   return (
   <Container fluid="md">
