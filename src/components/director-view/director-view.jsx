8ad47fe08a9f8a3a35af42621f351e14168d62c5
Dr. Lawrence Ibeh <drlawrenceibeh@gmail.com>
2021-12-29 15:54:29 +0100

    update-director-view

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
+
+import "./director-view.scss";
+
+export class DirectorView extends React.Component {
+    render() {
+        const { Director, onBackClick, movies } = this.props;
+
+        return (
+            <Container>
+                <br />
+                <Card align="center">
+                    <h4>Director</h4>
+                    <Card.Body>
+                        <div>
+                            <span className="label">Name: </span>
+                            <span className="value">{Director.Name}</span>
+                        </div>
+                        <div>
+                            <span className="label"> Description: </span>
+                            <span className="value">{Director.Description}</span>
+                        </div>
+                        <div>
+                            <span className="label">Born: </span>
+                            <span className="value">{Director.Birthday}</span>
+                        </div>
+                        <div>
+                            <span className="label">Death: </span>
+                            <span className="value">{Director.Death}</span>
+                        </div>
+                        <br />
+                        <div className="backButton">
+                            <Button size="md" variant="outline-primary" onClick={() => { onBackClick(null); }}>Back</Button>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </Container>
+        );
+    }
+}
+
+DirectorView.proptypes = {
+    Director: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Birthday: PropTypes.number,
+        Death: PropTypes.number,
+    }).isRequired,
+};
